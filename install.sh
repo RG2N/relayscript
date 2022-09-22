573a930904869612d36c1cf84ae11a65bc47bb42
RG2N <64289295+RG2N@users.noreply.github.com>
2022-09-22 18:11:21 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,5 @@ apt upgrade
 apt install default-jre
 java -version
 wget https://github.com/lax1dude/eaglercraft/blob/main/stable-download/sp-relay.jar
+wget https://github.com/RG2N/relayscript/raw/main/relayConfig.ini
+wget 
